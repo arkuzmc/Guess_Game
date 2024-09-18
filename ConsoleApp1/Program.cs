510df3cbdf67e64911c3d0df45f0299340922785
Enes Arkın <Enes Arkn@DESKTOP-VMGKG2U>
2024-09-18 13:37:19 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rastgele = new Random();
+            int choosennumber = rastgele.Next(1, 100);
+            int guess,attempts = 5;
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("     WELCOME TO GUESS GAME!!!      ");
+            Console.WriteLine("===================================");
+
+            Console.WriteLine("Enter a number between one and one hundred");
+            while (attempts > 0)
+            {
+                Console.WriteLine("Please Enter Your Guess");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > choosennumber)
+                {
+                    Console.WriteLine("You entered a number bigger than The selected number");
+                }
+                else if (guess < choosennumber)
+                {
+                    Console.WriteLine("You entered a number smaller than The selected number");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations, You Found The Number");
+                    break;
+                }
+
+                attempts--;
+                if (attempts > 0)
+                {
+                    Console.WriteLine("Kalan Hakkınız: " + attempts);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry Your Chances İs Over.: " + choosennumber);
+                }
+
+                Console.WriteLine("GAME OVER.");
+
+
+            }
+
+        }
+    }
+}
